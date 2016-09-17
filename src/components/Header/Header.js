2c7268917361d317c567b4bc09546f768c84e1e6
Seth Reid <contact@sethreid.co.nz>
2016-09-18 10:30:20 +1200

    made header into the main nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,55 @@
-import React from 'react'
-import { IndexLink, Link } from 'react-router'
-import './Header.scss'
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import onClickOutside from 'react-onclickoutside'
+import { Link } from 'react-router'
 
-export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
-  </div>
-)
+class Header extends Component {
+  constructor () {
+    super()
+    this.state = {
+      sideNavShow: false
+    }
+    this.toggleSideNav.bind(this)
+  }
+  toggleSideNav () {
+    this.setState({
+      sideNavShow: !this.state.sideNavShow
+    })
+  }
+  handleClickOutside () {
+    this.setState({
+      sideNavShow: false
+    })
+  }
+  render () {
+    const sideNavClasses = classNames({
+      'side-nav': true,
+      'side-nav-show': this.state.sideNavShow,
+      'side-nav-hide': !this.state.sideNavShow
+    })
+    return (
+      <nav>
+        <div className='nav-wrapper black'>
+          <div className='container'>
+            <Link to='/' className='brand-logo'>Employee CV Manager</Link>
+            <a onClick={() => this.toggleSideNav()} className='button-collapse'>
+              <i className='material-icons'>menu</i>
+            </a>
+            <ul className='main-navigation right hide-on-med-and-down'>
+              <li className='active'><a href='#'>Employees</a></li>
+              <li><a href='#'>My Profile</a></li>
+              <li><a href='#'>Settings</a></li>
+            </ul>
+            <ul className={sideNavClasses}>
+              <li className='active'><a href='#'>Employees</a></li>
+              <li><a href='#'>My Profile</a></li>
+              <li><a href='#'>Settings</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
 
-export default Header
+export default onClickOutside(Header)
