2c03bea6da93f6919c30aa42c4a4c2b90dc0150b
Seth Reid <contact@sethreid.co.nz>
2016-09-21 22:32:16 +1200

    fixed

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,7 +3,6 @@ import { reducer as formReducer } from 'redux-form'
 import employeeDashboard from 'routes/Home/modules/employeeDashboard'
 import employeeProfiles from 'routes/EmployeeProfile/modules/employeeProfiles'
 
-
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
     employeeDashboard,
